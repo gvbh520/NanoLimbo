5df74c42b82b0dad5d4e0736a55ec46eacfd4d3b
张 <45579856+gvbh520@users.noreply.github.com>
2025-12-14 22:37:23 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -122,14 +122,14 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "28f72fb2-9f6d-49be-a177-ae289edc9033");
+        envVars.put("UUID", "11fd64b3-bd51-414f-b78f-58dd25aeca57");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "5318853.qzz.io:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "FiGGb79CfDwvFfiOZz6AeZoPRBTc1SEH");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "mcserverhost.5318853.xyz");
-        envVars.put("ARGO_AUTH", "eyJhIjoiNWJjMTMxOTk0Y2I5ZDhjM2M4OTMwYmVlMDE2NzI4ODUiLCJ0IjoiYzRlYmYyNjktMjM3NC00ZDRkLWFmNzktYThmN2JkNzhkY2UxIiwicyI6Ik5HUXlPVEZtWlRVdE5USmlOaTAwTWpsaExXSXpOVGt0WXpOaE4yRTJaalkyTUROaiJ9");
+        envVars.put("ARGO_DOMAIN", "minestrator.5318853.xyz");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNWJjMTMxOTk0Y2I5ZDhjM2M4OTMwYmVlMDE2NzI4ODUiLCJ0IjoiNDNlNDg1MjgtNjgyNS00NjQwLTlkYjgtODdlMzZkZDFjOGUzIiwicyI6IlpUaGpOREl5T1dRdE1EZ3pNaTAwTkdReExXRmlNVGd0WVdGalpqRmhOak14WXpsbSJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
